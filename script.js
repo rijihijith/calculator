7225ad8fced7889814812331fa5cd259c6d1cb0d
riji <rijihijith@gmail.com>
2022-01-06 11:24:49 +0530

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+function displayNUM(num)
+{
+    
+   result.value+=num
+}
+function clr(){
+    result.value = ""
+}
+function solve(){
+
+    result.value=eval(result.value)
+}
+function del(num){
+
+    result.value=result.value.slice(0,-1)
+}
